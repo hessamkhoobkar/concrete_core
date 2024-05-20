e89848cae4214e8c952d397cac14c807c56e85c2
hessam_khoobkar <amirhessam.dev@gmail.com>
2024-05-20 04:28:02 +0330

    format imports

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@/components/ui/button";
 import { login } from "./actions";
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
